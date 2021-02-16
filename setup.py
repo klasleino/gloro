6ce101df57200534abe72d2acd4db93e88ea2119
Klas Leino <kleino@live.com>
2021-02-16 17:41:50 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 with open('requirements.txt', 'r') as requirements:
     setup(
         name='gloro',
-        version='0.0.1',
+        version='0.0.2',
         install_requires=list(requirements.read().splitlines()),
         packages=find_packages(),
         description=
@@ -15,6 +19,6 @@ with open('requirements.txt', 'r') as requirements:
             'Programming Language :: Python :: 3',
             'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent'],
-        long_description='file: README.md',
+        long_description=long_description,
         long_description_content_type='text/markdown'
     )

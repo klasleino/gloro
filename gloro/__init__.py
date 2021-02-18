8c7df509a2ea922a59f11a2325738a1d68788bec
Klas Leino <kleino@live.com>
2021-02-17 22:32:11 -0500

    get version number automatically

--- gloro/__init__.py
+++ gloro/__init__.py
@@ -1,3 +1,5 @@
+__version__ = '0.0.4'
+
 # Disable eager execution if using Tensorflow 2.
 try:
     from tensorflow.python.framework.ops import disable_eager_execution
--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
+import os
+
 from setuptools import setup, find_packages
 
 
-with open("README.md", "r", encoding="utf-8") as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            return line.split("'")[1]
+
+    raise RuntimeError('Unable to find version string.')
 
 with open('requirements.txt', 'r') as requirements:
     setup(
         name='gloro',
-        version='0.0.3',
+        version=get_version('gloro/__init__.py'),
         install_requires=list(requirements.read().splitlines()),
         packages=find_packages(),
         description=

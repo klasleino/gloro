70f580e0544e5c71960523e45a5664472045d287
Klas Leino <kleino@live.com>
2021-02-17 22:31:49 -0500

    add option for sparse labels

--- gloro/training/losses.py
+++ gloro/training/losses.py
@@ -3,32 +3,43 @@ import tensorflow.keras.backend as K
 
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.losses import Loss
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
 
 from gloro.training.utils import add_extra_column
 
 
 class Crossentropy(Loss):
 
-    def __init__(self, **kwargs):
+    def __init__(self, sparse=False, **kwargs):
         super().__init__()
 
         kwargs['from_logits'] = True
 
-        self._loss = CategoricalCrossentropy(**kwargs)
+        if sparse:
+            self._loss = SparseCategoricalCrossentropy(**kwargs)
+        else:
+            self._loss = CategoricalCrossentropy(**kwargs)
+
+        self._sparse = sparse
 
     def call(self, y_true, y_pred):
         # Add an extra column of zeros for the bottom class.
-        y_true = add_extra_column(y_true)
+        if not self._sparse:
+            y_true = add_extra_column(y_true)
 
         return self._loss.call(y_true, y_pred)
 
     def get_config(self):
-        return self._loss.get_config()
+        config = self._loss.get_config()
+        config.update({
+            'sparse': self._sparse
+        })
+        return config
 
 
 class Trades(Loss):
 
-    def __init__(self, lam=1., robust_loss_string=None):
+    def __init__(self, lam=1., sparse=False, robust_loss_string=None):
         super().__init__()
 
         self._lam = tf.Variable(lam, trainable=False)
@@ -36,13 +47,18 @@ class Trades(Loss):
 
         self._robust_loss_string = robust_loss_string
 
-        self._crossentropy = CategoricalCrossentropy(from_logits=True)
+        if sparse:
+            self._crossentropy = SparseCategoricalCrossentropy(from_logits=True)
+        else:
+            self._crossentropy = CategoricalCrossentropy(from_logits=True)
 
         if robust_loss_string is None:
-            self._robust_loss = self._crossentropy
+            self._robust_loss = CategoricalCrossentropy(from_logits=False)
 
         else:
             self._robust_loss = tf.losses.get(robust_loss_string)
+
+        self._sparse = sparse
 
     @property
     def lam(self):
@@ -54,21 +70,26 @@ class Trades(Loss):
 
     def call(self, y_true, y_pred):
         # Add an extra column of zeros for the bottom class.
-        y_true = add_extra_column(y_true)
+        if not self._sparse:
+            y_true = add_extra_column(y_true)
 
         # Encourage predicting the correct class, even non-robustly.
-        standard_loss = self._crossentropy(y_true[:, :-1], y_pred[:, :-1])
+        if self._sparse:
+            standard_loss = self._crossentropy(y_true, y_pred[:, :-1])
+        else:
+            standard_loss = self._crossentropy(y_true[:, :-1], y_pred[:, :-1])
 
         # Encourage predicting robustly, even incorrectly. We take the robust
         # loss but using the model's prediction as the ground truth.
         new_ground_truth = add_extra_column(tf.nn.softmax(y_pred[:, :-1]))
 
-        robust_loss = self._robust_loss(new_ground_truth, y_pred)
+        robust_loss = self._robust_loss(new_ground_truth, tf.nn.softmax(y_pred))
 
         return standard_loss + self._lam * robust_loss
 
     def get_config(self):
         return {
             'lam': self.lam,
-            'robust_loss_string': self._robust_loss_string
+            'robust_loss_string': self._robust_loss_string,
+            'sparse': self._sparse
         }
--- gloro/training/metrics.py
+++ gloro/training/metrics.py
@@ -2,11 +2,22 @@ import tensorflow as tf
 
 
 def clean_acc(y_true, y_pred):
+    labels = tf.case(
+        [(tf.equal(tf.shape(y_true)[1], 1), 
+            lambda: tf.cast(y_true, 'int32'))],
+        lambda: tf.cast(tf.argmax(y_true, axis=1), 'int32'))
+
     return tf.reduce_mean(tf.cast(
-        tf.equal(tf.argmax(y_true, axis=1), tf.argmax(y_pred[:, :-1], axis=1)),
+        tf.equal(labels, tf.cast(tf.argmax(y_pred[:, :-1], axis=1), 'int32')),
         'float32'))
 
+
 def vra(y_true, y_pred):
+    labels = tf.case(
+        [(tf.equal(tf.shape(y_true)[1], 1), 
+            lambda: tf.cast(y_true, 'int32'))],
+        lambda: tf.cast(tf.argmax(y_true, axis=1), 'int32'))
+
     return tf.reduce_mean(tf.cast(
-        tf.equal(tf.argmax(y_true, axis=1), tf.argmax(y_pred, axis=1)),
+        tf.equal(labels, tf.cast(tf.argmax(y_pred, axis=1), 'int32')),
         'float32'))

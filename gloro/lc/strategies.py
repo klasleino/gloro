fix numerical instability in normalization (#10)
fixes #8

gloro/lc/strategies.py
@@ -199,9 +199,15 @@ class PowerMethod(LipschitzComputationStrategy):
         # Update the power iterate.
         self.iterate.assign(x)
 
+        # If ||x|| is 0, add a small constant to the denominator to prevent
+        # dividing by zero.
+        optional_eps = gloro.constants.EPS * tf.cast(
+            tf.equal(tf.reduce_sum(x**2.), 0.),
+            dtype='float32'
+        )
         return tf.sqrt(
             tf.reduce_sum((self._forward(x))**2.) / (
-                tf.reduce_sum(x**2.) + gloro.constants.EPS
+                tf.reduce_sum(x**2.) + optional_eps
             )
         )
 

gloro/utils.py
@@ -37,9 +37,10 @@ def get_value(x):
 
 
 def l2_normalize(x, axis=None):
-    return x / (
-        tf.sqrt(tf.reduce_sum(x**2., axis=axis, keepdims=True)) +
-            gloro.constants.EPS)
+    raw_norm = tf.sqrt(tf.reduce_sum(x**2., axis=axis, keepdims=True))
+    norm = tf.where(raw_norm > 0, raw_norm, raw_norm + gloro.constants.EPS)
+
+    return x / norm
 
 
 def print_if_verbose(verbose):